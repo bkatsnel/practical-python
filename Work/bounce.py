866ddeb59f7b2211c3f24ad5de46fbcb9ace4394
1Copenut <pierce.trevor@gmail.com>
2020-09-29 07:07:20 -0500

    First commit

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,11 @@
 # bounce.py
 #
 # Exercise 1.5
+height = 100  # meters
+bounces = 0  # number of bounces
+
+while bounces < 10:
+    bounces = bounces + 1
+    height = height * (3/5)
+    # print(height)
+    print(round(height, 4))

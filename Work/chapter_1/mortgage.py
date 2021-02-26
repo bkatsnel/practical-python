3858617e02d54b6035b0d402db45999ace12c804
1Copenut <pierce.trevor@gmail.com>
2021-02-26 07:20:43 -0600

    Updating the mortgage calculator with formatted strings
    Changes to be committed:
    * modified:   .gitignore
    * modified:   Work/chapter_1/mortgage.py

--- Work/chapter_1/mortgage.py
+++ Work/chapter_1/mortgage.py
@@ -1,6 +1,7 @@
 # mortgage.py
 
 principal = 500000.0
+starting_principal = 500000.0
 rate = 0.05
 payment = 2684.11
 total_paid = 0.0
@@ -17,7 +18,9 @@ while principal > 0:
         principal = principal * (1+rate/12) - payment
         total_paid = total_paid + payment
     payments_made = payments_made + 1
-    print(payments_made, round(total_paid, 2), round(principal, 2))
+    # print(payments_made, round(total_paid, 2), round(principal, 2))
+    print(f'{payments_made:3n} {total_paid:10.2f} {principal:10.2f}')
 
-print('Total paid', round(total_paid, 2))
-print('Months', payments_made)
+print(f'Principal borrowed: ${starting_principal:.2f}')
+print(f'Total paid: ${total_paid:.2f}')
+print(f'Months to pay off: {payments_made:n}')

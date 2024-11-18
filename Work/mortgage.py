3e14af1710e7d9f494a17c81ef2257801a7fb11a
Dostonbek Rustamov <doston_ra@Dostonbeks-MacBook-Air.local>
2024-11-18 19:30:38 +0000

    Completed exercise 1.10 and saved it to mortgage.py

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -7,21 +7,26 @@ principal = 500000.0
 rate = 0.05
 payment = 2684.11
 total_paid = 0.0
-month=0
-new_payment = 3684.11
+month = 0.0
+
+#User-defined extra payment variables
+extra_payment_start_month=61    # Extra payments start after 5 years (month 61)
+extra_payment_end_month = 108   # Extra payments end after 4 years (month 108)
+extra_payment = 1000			# Extra payment amount
 while principal > 0:
-	if month < 12: # First 12 months with extra payment
-		principal = principal * (1+rate/12) - new_payment
-		total_paid = total_paid + new_payment
-	else: # Regular payment after 12 months
-		principal = principal * (1+rate/12) - payment
-		total_paid = total_paid + payment
-	month = month + 1
-	
-	# Final Adjustment if the principal goes negative
+	# Chack if the current month is within the extra payment period
+	if extra_payment_start_month <= month < extra_payment_end_month:
+		monthly_payment = extra_payment + payment
+	else:
+		monthly_payment = payment
+	# Apply monthly interest and substract the payment
+	principal = principal * (1+rate/12) - payment
+	total_paid += monthly_payment
+	month +=1
+	# Final adjustment if the principal goes negative
 	if principal < 0:
 		total_paid = total_paid + principal # Adjust the last payment
 		principal = 0 # Set principal to 0 to exit the loop
-print("Total Paid: ", round(total_paid, 2))
-print("Months spent:", month)
+	print(month, "   ", total_paid)
+	
 
